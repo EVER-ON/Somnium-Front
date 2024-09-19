b51d3e87b9e8e728a4f6da6b3c4b4f09860c62eb
Radon24 <radon24code@gmail.com>
2024-09-19 11:21:21 +0300

    feat: Created vue router

--- src/app/router.js
+++ src/app/router.js
@@ -1,18 +1,32 @@
+import {computed, ref} from "vue";
 import { createRouter, createWebHistory } from 'vue-router';
 import MainPage from '@/pages/main-page.vue';
 import BlogPage from '@/features/blog/blog.vue';
 import NewsPage from '@/features/news/news.vue';
+import WikiPage from "@/pages/wiki-page.vue";
 
-const routes = [
-    { path: '/', name: 'MainPage', component: MainPage },
+const routes= [
+    { path: '/main', name: 'MainPage', component: MainPage },
+    { path: '/wiki', name: 'WikiPage', component: WikiPage },
     { path: '/blog', name: 'BlogPage', component: BlogPage },
     { path: '/news', name: 'NewsPage', component: NewsPage },
-    {path: '/recources', name: 'Resources'}
-];
+    {path: '/resources', name: 'Resources'}
+]
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+    currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+    return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
 
 export default router;
