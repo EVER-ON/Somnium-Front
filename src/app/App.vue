bc903cb35939b7dea21b0802593383f295ae523d
EVER-ON <gurkadima2004@bk.ru>
2024-09-19 20:02:45 +0300

    feat: Make a background for the main fix: Overflow-x bug fix

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
+    <!-- Используем v-show, чтобы только скрыть элемент, оставляя его в DOM -->
+    <div v-show="isMainRoute" class="background-image"></div>
     <Header />
-    <router-view />
+    <main>
+      <router-view />
+    </main>
     <Footer />
   </div>
 </template>
@@ -9,20 +13,55 @@
 <script>
 import Header from '@/shared/ui/Header.vue';
 import Footer from '@/shared/ui/Footer.vue';
+import {computed} from 'vue';
+import {useRoute} from 'vue-router';
 
 export default {
   components: {
     Header,
     Footer,
   },
+  setup() {
+    const route = useRoute();
+
+    const isMainRoute = computed(() => route.path === '/main');
+
+    return {
+      isMainRoute,
+    };
+  }
 };
 </script>
 
 <style scoped>
+
+html, body{
+  min-width: 100%;
+  margin: 0;
+}
 #app {
+
+  min-width: 100%;
   display: flex;
   flex-direction: column;
   min-height: 100vh; /* Ensure the app takes at least the full viewport height */
+
+
+  overflow-x: hidden/* Ensure the background image is positioned relative to this container */
+
+}
+
+.background-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh; /* Cover the whole viewport height */
+  background-image: url("@/resources/images/backgroundIce.png"); /* Укажите путь к вашему изображению */
+  background-repeat: no-repeat;
+  background-position: center; /* Центрирование изображения */
+  background-size: cover; /* Сделать изображение адаптивным */
+  z-index: -1; /* Поместить фоновое изображение под другими элементами */
 }
 
 header {
@@ -30,7 +69,8 @@ header {
 }
 
 main {
-   /* Allow the main content area to grow and take available space */
+  position: relative;
+  min-height: 100vh;
 }
 
 footer {
